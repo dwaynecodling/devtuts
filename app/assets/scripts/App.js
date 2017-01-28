068b4cb5fd51b434ed812c363137fe853582ad23
fitsydwag <dwayneandecodling@gmail.com>
2017-01-28 17:44:32 +0000

    udpased with webpack reloaded

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,11 @@
+var $ = require('jquery');
+var Person = require('./modules/Person');
+
+
+alert("Testing 123  it works ok");
+
+var john = new Person("John Doe", "blue");
+john.greet();
+
+var jane = new Person("Jane Smith", "green");
+jane.greet();
--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,16 +1,14 @@
-
 var gulp = require('gulp'),
 postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
 cssimport = require('postcss-import'),
-mixins = require('postcss-mixins'),
-hexrgba = require('postcss-hexrgba');
+mixins = require('postcss-mixins')
 
 gulp.task('styles', function(){
   return gulp.src('./app/assets/styles/styles.css')
-  .pipe(postcss([cssimport, mixins,cssvars, nested, hexrgba, autoprefixer]))
+  .pipe(postcss([cssimport, mixins,cssvars, nested, autoprefixer]))
   .on('error', function(errorInfo) {
     console.log(errorInfo.toString());
     this.emit('end');
